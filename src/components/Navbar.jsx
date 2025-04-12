65dd800442733d7aeafdb113dddefa44736a950c
Case1490 <renzoramos414@gmail.com>
2025-04-12 18:55:15 -0500

    sobre mi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,35 @@
 import Logo from "../assets/logo.png";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
+  const [shouldShowShadow, setShouldShowShadow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setShouldShowShadow(true);
+      } else {
+        setShouldShowShadow(false);
+      }
+    };
+
+    // Agregar evento de scroll al cargar el componente
+    document.addEventListener("scroll", handleScroll);
+
+    // Limpiar el evento de scroll al desmontar el componente
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // Clase condicional para el div fijo que muestra la sombra
+  const navbarClass = shouldShowShadow
+    ? "fixed w-full bg-white z-[1000] shadow-xl"
+    : "fixed w-full bg-white z-[1000]";
+
   return (
-    <div className="fixed w-full bg-white z-[1000]">
-      <div className=" w-5/6 m-auto flex items-center justify-between py-2">
+    <div className={navbarClass}>
+      <div className="w-5/6 m-auto flex items-center justify-between py-2">
         <div className="w-[180px]">
           <img src={Logo} alt="Logo" className="w-full" />
         </div>
@@ -14,7 +40,7 @@ const Navbar = () => {
             <li>Sobre mí</li>
             <li>Servicios</li>
             <li>Testimonios</li>
-            <li className=" bg-CyanMain text-white py-2 px-3 rounded-full">
+            <li className="bg-CyanMain text-white py-2 px-3 rounded-full">
               Contacto
             </li>
           </ul>
--- src/sections/AboutOfMe.jsx
+++ src/sections/AboutOfMe.jsx
@@ -1,13 +1,74 @@
+import PhotoDrCardenas from "../assets/fotos/CALADO 10.png";
+import CirclePurple from "../assets/circle.png";
+
+// IMPORT OF ICONS
+import { BsFileEarmarkMedicalFill } from "react-icons/bs";
+import { FaBriefcaseMedical } from "react-icons/fa6";
+import { RiTeamFill } from "react-icons/ri";
+import { IoHappy } from "react-icons/io5";
+
 const AboutOfMe = () => {
+  const dataItems = [
+    {
+      icon: <BsFileEarmarkMedicalFill size={25} />,
+      text: "Más 20 años de experiencia quirúrgica",
+    },
+    {
+      icon: <FaBriefcaseMedical size={25} />,
+      text: "Especialista en oncología quirúrgica",
+    },
+    {
+      icon: <RiTeamFill size={25} />,
+      text: "Trabajo en equipo multidisciplinario",
+    },
+    {
+      icon: <IoHappy size={25} />,
+      text: "Cientos de pacientes recuperados",
+    },
+  ];
+
   return (
-    <div>
-      <h1>Sobre míxd</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-        dolorum vero iure. Voluptas in iusto, dolorem magni harum commodi libero
-        dicta accusantium nihil labore voluptates. Earum quaerat rem voluptates
-        reprehenderit.
-      </p>
+    <div className="w-5/6 m-auto flex items-center justify-around py-8">
+      <div className="relative rounded-full overflow-hidden w-[590px] h-[500px]">
+        <img
+          src={CirclePurple}
+          alt=""
+          className="w-full h-[580px] absolute inset-0"
+        />
+        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[300px]">
+          <img src={PhotoDrCardenas} alt="Dr Cardenas" className="w-full" />
+        </div>
+      </div>
+      <div className="w-1/2">
+        <h1 className="text-3xl font-bold mb-2">
+          Entre Tijeras y Esperanzas: La Trayectoria Quirúrgica de un Héroe
+          Contra el Cáncer
+        </h1>
+        <p className=" text-justify">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          dolorum vero iure. Voluptas in iusto, dolorem magni harum commodi
+          libero dicta accusantium nihil labore voluptates. Earum quaerat rem
+          voluptates reprehenderit. Voluptas in iusto, dolorem magni harum
+          commodi libero dicta accusantium nihil labore voluptates. Earum
+          quaerat rem voluptates reprehenderit.
+        </p>
+
+        <div className="mt-8 grid grid-cols-2 gap-4">
+          {dataItems.map((item, index) => (
+            <div
+              key={index}
+              className="flex items-center space-x-2 border py-2 px-2 rounded-xl shadow-xl w-[290px] bg-PurpleMain"
+            >
+              <div className="bg-PinkMain text-white p-4 rounded-full">
+                {item.icon}
+              </div>
+              <div>
+                <p className=" text-lg text-white">{item.text}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
